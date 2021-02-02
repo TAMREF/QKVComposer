e71de7f2ffb15f87ece540b073b6367f7bc6f3d3
jhhope1 <jhhope1@naver.com>
2021-02-03 02:04:07 +0900

    add time and note acc to tensorboard log

--- model/model.py
+++ model/model.py
@@ -29,33 +29,41 @@ class Baseline(pl.LightningModule):
         x, target = batch
         logits = self.model(x)
         loss = self.loss(logits, target)
-        acc = get_accuracy(logits[0], target[0])
+        note_acc = get_accuracy(logits[0], target[0])
+        time_acc = get_accuracy(logits[1], target[1])
         return {
             'val_loss': loss,
-            'val_acc': acc
+            'val_note_acc': note_acc,
+            'val_time_acc': time_acc
         }
     
     def validation_epoch_end(self, outputs):
         loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        acc = torch.stack([x['val_acc'] for x in outputs]).mean()
+        note_acc = torch.stack([x['val_note_acc'] for x in outputs]).mean()
+        time_acc = torch.stack([x['val_time_acc'] for x in outputs]).mean()
         self.log('val_loss', loss)
-        self.log('val_acc', acc)
+        self.log('val_note_acc', note_acc)
+        self.log('val_time_acc', time_acc)
     
     def test_step(self, batch, batch_idx):
         x, target = batch
         logits = self.model(x)
         loss = self.loss(logits, target)
-        acc = get_accuracy(logits[0], target[0])
+        note_acc = get_accuracy(logits[0], target[0])
+        time_acc = get_accuracy(logits[1], target[1])
         return {
             'test_loss': loss,
-            'test_acc': acc
+            'test_note_acc': note_acc,
+            'test_time_acc': time_acc
         }
 
     def test_epoch_end(self, outputs):
         loss = torch.stack([x['test_loss'] for x in outputs]).mean()
-        acc = torch.stack([x['test_acc'] for x in outputs]).mean()
+        note_acc = torch.stack([x['test_note_acc'] for x in outputs]).mean()
+        time_acc = torch.stack([x['test_time_acc'] for x in outputs]).mean()
         self.log('test_loss', loss)
-        self.log('test_acc', acc)
+        self.log('test_note_acc', note_acc)
+        self.log('test_time_acc', time_acc)
 
     def configure_optimizers(self):
         if self.cfg.train.optim == 'adam':
